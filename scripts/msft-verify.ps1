f664a16c585332e93242bed5c335fe372c2b584c
ms-mikeb <mibir@microsoft.com>
2022-05-24 15:35:43 -0600

    change to download script

--- scripts/msft-verify.ps1
+++
@@ -1,3 +0,0 @@
-$url = "https://raw.githubusercontent.com/NuGet/NuGetGallery/master/src/VerifyMicrosoftPackage/verify.ps1"
-Invoke-WebRequest $url -OutFile verify.ps1
-.\verify.ps1 .\unsigned\*.nupkg
